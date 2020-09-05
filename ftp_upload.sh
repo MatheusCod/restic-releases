c1f4bc3de7cbae525c563c99a79132ecd2d198b3
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-05 13:57:22 -0300

    Update ftp_upload.sh

--- ftp_upload.sh
+++ ftp_upload.sh
@@ -21,10 +21,8 @@ then
   sudo mv empacotar-deb.sh $LOCALPATH
   sudo mv empacotar-rpm.sh $LOCALPATH
   cd $LOCALPATH
-  ls
   sudo ./empacotar-deb.sh restic restic-$github_version $github_version " "
   sudo ./empacotar-rpm.sh restic restic-$github_version $github_version " " "restic is a program that does backups right"
-  ls
   if [ $github_version > $ftp_version ]
   then
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/restic-$github_version-ppc64le.deb"
