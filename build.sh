803d02908ff13ed568369bf100197a1a77518628
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-05 02:15:39 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,25 @@
 
 set -e
 github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
 
-echo "1"
-wget https://github.com/restic/restic/releases/download/v$github_version/restic-$github_version.tar.gz
-echo "2"
-tar -xzf restic-$github_version.tar.gz
-echo "3"
-mv restic-$github_version restic
-echo "4"
-cd restic
-echo "5"
-make all
-echo "6"
-mkdir output
-echo "7"
-mv restic output/restic-$github_version
-echo "8"
-ls -la output
-echo "9"
+if [ $github_version != $ftp_version ]
+then
+  echo "1"
+  wget https://github.com/restic/restic/releases/download/v$github_version/restic-$github_version.tar.gz
+  echo "2"
+  tar -xzf restic-$github_version.tar.gz
+  echo "3"
+  mv restic-$github_version restic
+  echo "4"
+  cd restic
+  echo "5"
+  make all
+  echo "6"
+  mkdir output
+  echo "7"
+  mv restic output/restic-$github_version
+  echo "8"
+  ls -la output
+  echo "9"
+fi
